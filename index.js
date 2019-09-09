2585b00dbf6213cbbdcaea21bfa91ba7d04d4447
Javier Garay <javier.garay@2brains.cl>
2019-09-09 19:44:13 -0300

    validacion token

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const request = require("request");
 var cache = require("express-redis-cache")();
+var admin = require('firebase-admin');
 
 const app = express();
+var credencialFirebase = require('./firebase.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(credencialFirebase),
+    databaseURL: 'https://desafioRipley.firebaseio.com'
+  });
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -13,7 +21,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get("/productos", cache.route({expire: 120}), function(req, res) {
+app.get("/productos", cache.route({ expire: 120 }), function(req, res) {
   request(
     "https://simple.ripley.cl/api/v2/products?partNumbers=2000374667876P,2000372444561P,2000375722154P,2000369724997P,MPM00002893384,2000374667883P,2000374667913P,2000375441888P,2000371726989P",
     function(error, response, body) {
@@ -24,6 +32,20 @@ app.get("/productos", cache.route({expire: 120}), function(req, res) {
   );
 });
 
+app.post("/validarToken", function(req, res) {
+  // idToken comes from the client app
+  console.log("req: "+req);
+  admin
+    .auth()
+    .verifyIdToken(req.body.idToken)
+    .then(function(decodedToken) {
+      let uid = decodedToken.uid;
+      // ...
+    })
+    .catch(function(error) {
+      // Handle error
+    });
+});
 
 app.listen(3000, () => {
   console.log("El servidor está inicializado en el puerto 3000");
