d824c59e6ebe77a6eda5fd8633e52a1559c9b5d1
Javier Garay <javier.garay@2brains.cl>
2019-09-09 17:24:13 -0300

    se agrega redis al get de productos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const request = require("request");
+var cache = require("express-redis-cache")();
 
 const app = express();
 
@@ -12,15 +13,11 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get("/productos", function(req, res) {
-  console.log("en funcion get 0");
+app.get("/productos", cache.route({expire: 120}), function(req, res) {
   request(
     "https://simple.ripley.cl/api/v2/products?partNumbers=2000374667876P,2000372444561P,2000375722154P,2000369724997P,MPM00002893384,2000374667883P,2000374667913P,2000375441888P,2000371726989P",
     function(error, response, body) {
-      console.log("en funcion get 1");
       if (!error && response.statusCode == 200) {
-        console.log("en funcion get 2");
-        res.setHeader("Access-Control-Allow-Origin", "*");
         res.send(body);
       }
     }
